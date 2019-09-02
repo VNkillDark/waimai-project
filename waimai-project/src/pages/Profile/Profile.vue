2542e1a3cafb8f5ac91fa7f6ca40fb0e419ff7ba
HeQinYi <heqinyi24@163.com>
2019-09-02 18:20:34 +0800

    stude_day1

---
+++ waimai-project/src/pages/Profile/Profile.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Profile
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+ <!-- stylus 预处理器 <-->
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
